4f9cdca198930513f45ef20cc92cd046a2f4a779
Manash Kumar Mandal <manashmndl@gmail.com>
2016-11-05 18:06:39 +0600

    huge update

--- src/Automator/User.java
+++ src/Automator/User.java
@@ -3,6 +3,8 @@ import com.jaunt.Elements;
 import com.jaunt.JauntException;
 import com.jaunt.UserAgent;
 
+import java.util.ArrayList;
+
 
 /**
  * Created by Manash on 11/5/2016.
@@ -15,45 +17,44 @@ public class User {
     private UserAgent browser;
 
     private static final String LIBRARY_URL = "http://library.kuet.ac.bd";
-
-    private static final int MAX_COL = 6;
-    private static final int MAX_ROW = 3;
-
-    private static final int SERIAL_COL = 0;
-    private static final int BOOK_NAME_COL = 1;
-    private static final int AUTHOR_NAME_COL = 2;
-    private static final int CALL_NO_COL = 3;
-    private static final int DUE_DATE_COL = 4;
-    private static final int RENEWALS_COL = 5;
-
-    private static final int FIRST_ROW = 0;
-    private static final int SECOND_ROW = 1;
-    private static final int THIRD_ROW = 2;
-
     private static final String BOOKNAME_SELECTOR = "<td class=title>";
     private static final String RENEWALS_SELECTOR = "<span class=renewals>";
     private static final String DUE_DATE_SELECTOR = "<td class=date_due>";
     private static final String CALL_NO_SELECTOR = "<td class=call_no>";
+    private static final String LINK_SELECTOR = "<a>";
+    private static final String AUTHOR_SELECTOR = "<span class=item-details>";
 
-    private static final int MAX_ITEMS = 3;
 
     //Indices
     private static final int RENEWAL_INDEX = 1;
-    private static final int BOOKNAME_INDEX = 0;
-    private static final int AUTHOR_INDEX = 1;
 
-    private String[][] dataTable;
+
+    //Variables
+    private ArrayList<String> RenewalsLeft;
+    private ArrayList<String> BookNames;
+    private ArrayList<String> Authors;
+    private ArrayList<String> Calls;
+    private ArrayList<String> Dues;
+    private ArrayList<String> SerialNo;
+
+    private void init(){
+        browser = new UserAgent();
+        RenewalsLeft = new ArrayList<>();
+        Calls = new ArrayList<>();
+        Authors = new ArrayList<>();
+        Dues = new ArrayList<>();
+        SerialNo = new ArrayList<>();
+        BookNames = new ArrayList<>();
+    }
 
     public User () {
-        browser = new UserAgent();
-        dataTable = new String[MAX_ROW][MAX_COL];
+        init();
     }
 
     public User(String userID, String password){
         this.userID = userID;
         this.password = password;
-        browser = new UserAgent();
-        dataTable = new String[MAX_ROW][MAX_COL];
+        init();
     }
 
     public void login(){
@@ -86,68 +87,55 @@ public class User {
         return this.password;
     }
 
+    public void debug(){
+        for (int i = 0; i < BookNames.size(); i++){
+            System.out.println(" ===== DEBUG =====");
+            System.out.println("BOOK NAME: " + BookNames.get(i));
+            System.out.println("AUTHOR NAME: " + Authors.get(i));
+            System.out.println("DUE : " + Dues.get(i));
+            System.out.println("RENEWALS : " + RenewalsLeft.get(i));
+            System.out.println("CALL NO : " + Calls.get(i));
+            System.out.println();
+        }
+    }
+
     public void updateDetails(){
-        String[] renewalsLeft   = new String[MAX_ITEMS];
-        String[] bookNames      = new String[MAX_ITEMS];
-        String[] authors        = new String[MAX_ITEMS];
-        String[] calls          = new String[MAX_ITEMS];
-        String[] dues           = new String[MAX_ITEMS];
-        String[] serials        = new String[MAX_ITEMS];
 
-        Elements renewals       = browser.doc.findEvery(RENEWALS_SELECTOR);
-        Elements bookDetails    = browser.doc.findEvery(BOOKNAME_SELECTOR);
-        Elements dueDates       = browser.doc.findEvery(DUE_DATE_SELECTOR);
-        Elements callNo         = browser.doc.findEvery(CALL_NO_SELECTOR);
+        //Clearing the arrays
+        RenewalsLeft.clear();
+        Dues.clear();
+        SerialNo.clear();
+        BookNames.clear();
+        Calls.clear();
+        Authors.clear();
 
-        for (int i = 0; i < renewals.size(); i++) {
+        Elements renewalsElements       = browser.doc.findEvery(RENEWALS_SELECTOR);
+        Elements dueDatesElements       = browser.doc.findEvery(DUE_DATE_SELECTOR);
+        Elements callNoElements         = browser.doc.findEvery(CALL_NO_SELECTOR);
+        Elements bookNameElements       = browser.doc.findEvery(BOOKNAME_SELECTOR).findEvery(LINK_SELECTOR);
+        Elements authorElements         = browser.doc.findEvery(BOOKNAME_SELECTOR).findEvery(AUTHOR_SELECTOR);
 
+        System.out.println("Size: " + String.valueOf(renewalsElements.size()));
+
+        for (int i = 0; i < renewalsElements.size(); i++) {
             try {
-                // Getting number of available renewals
-                renewalsLeft[i] = extractRenewals(renewals.getElement(i).innerText());
-
-                // Getting due dates
-                dues[i] = dueDates.getElement(i).innerText();
-
-                // Getting the serials
-                serials[i] = String.valueOf(i + 1);
-
-                // Extract once
-                String authorBookString = bookDetails.getElement(i).innerText();
-
-                // Getting book name
-                bookNames[i] = extractBookName(authorBookString);
-
-                // Getting the author name
-                authors[i] = extractAuthor(authorBookString);
-
-                // Getting call numbers
-                calls[i] = callNo.getElement(i).innerText();
-
-                System.out.println(calls[i]);
-
+                RenewalsLeft.add(extractRenewals(renewalsElements.getElement(i).innerText()));
+                Dues.add(dueDatesElements.getElement(i).innerText());
+                SerialNo.add(String.valueOf(i + 1));
+                BookNames.add(bookNameElements.getElement(i).innerText().replaceAll(" /", "").replaceAll(" :", ""));
+                Calls.add(callNoElements.getElement(i).innerText());
+                Authors.add(authorElements.getElement(i).innerText().trim());
             } catch (JauntException e){
                 System.err.println(e);
             }
         }
-
     }
 
-
-    private String extractAuthor(String text){
-        String[] bookDetails = text.split("/");
-        return bookDetails[AUTHOR_INDEX].trim();
-    }
-
-    private String extractBookName(String text){
-        String[] bookDetails = text.split("/");
-        return bookDetails[BOOKNAME_INDEX].trim();
-    }
-
+    // Extracts the number of renewals available
     private String extractRenewals(String text){
         text = text.replace('(', ' ');
         String[] texts = text.split(" ");
         return texts[RENEWAL_INDEX];
     }
-
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,14 +9,24 @@ import Automator.User;
 
 public class Main extends Application {
 
+    String kundu = "051203026";
+    String kundupass = "Kundu123";
+    String kanakpass = "398381";
+
+
+    String manash = "051203043";
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
-        User user = new User("051203043", "051203043");
+//        User user = new User("051203026", "Kundu123");
+        User user = new User(kundu, kundupass);
         user.login();
         user.updateDetails();
+        user.debug();
 
         primaryStage.show();
     }
