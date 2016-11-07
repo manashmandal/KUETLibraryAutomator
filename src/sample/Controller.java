cb4b0cd759b24699d2072e541fb2ff66103f0f70
Manash Kumar Mandal <manashmndl@gmail.com>
2016-11-07 15:52:28 +0600

    added new ui files

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,57 @@
 package sample;
 
-public class Controller {
+import Automator.User;
+import com.sun.glass.events.MouseEvent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.StageStyle;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
+
+    private User user;
+
+    @FXML
+    private Pane mainPane;
+
+    @FXML
+    private HBox hBoxLayout;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event){
+        user.debug();
+    }
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private Label label;
+
+    public void setupBinding(StageStyle stageStyle){
+
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+//        String kundu = "051203026";
+//        String kundupass = "Kundu123";
+//        user = new User(kundu, kundupass);
+//        user.login();
+//        user.updateDetails();
+//        user.debug();
+
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,7 +7,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import Automator.User;
 
+
+
+
 public class Main extends Application {
+
+    final int max_width = 400;
+    final int min_width = 383;
+    final int max_height = 350;
+    final int min_height = 300;
+    final int height = 250;
 
     String kundu = "051203026";
     String kundupass = "Kundu123";
@@ -20,13 +29,19 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("KUET Library Automator");
+        Scene scene = new Scene(root);
+        primaryStage.setMaxWidth(max_width);
+        primaryStage.setMinWidth(min_width);
+        primaryStage.setMaxHeight(max_height);
+        primaryStage.setMinHeight(min_height);
+        primaryStage.setScene(scene);
+//        primaryStage.setScene(new Scene(root, max_width, height));
 //        User user = new User("051203026", "Kundu123");
-        User user = new User(kundu, kundupass);
-        user.login();
-        user.updateDetails();
-        user.debug();
+//        User user = new User(kundu, kundupass);
+//        user.login();
+//        user.updateDetails();
+//        user.debug();
 
         primaryStage.show();
     }
